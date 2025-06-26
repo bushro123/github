61053816affbd83f0f0ddbd2db7238364f7fb21b
bushro123 <fadumocadceed34@gmail.com>
2025-06-26 20:46:39 +0300

    dhalashadayda

--- github/Program.cs
+++ github/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string name = "fadumo c/naasir cadceed";
+            Console.WriteLine("Name: " + name);
+string mothername = "faizo Ali";
+Console.WriteLine("Momother'Name: " + mothername);
+string dop = "2004-01-16";
+Console.WriteLine("Date Of Birth: " + dop);
+string pob = "Galkio";
+Console.WriteLine("Place Of Pirt: " + pob);
+string address = "Garsoor";
+Console.WriteLine("Address: " + address);
+int telphone = 096892044;
+Console.WriteLine("Telphone: " + telphone);
+string sex = "Female";
+Console.WriteLine("Sex: " + sex);
+string status = "Single";
+Console.WriteLine("Status: " + status);
+
